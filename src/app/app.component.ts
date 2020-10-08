2f618551595ca9bfba3c5afc48a3f333cbd5fa17
Jonathan Perry <jpper@umich.edu>
2020-10-08 14:48:40 +0900

    Upgraded angular to 10 and added in rate-app to application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Platform } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
+import { AppRate } from "@ionic-native/app-rate/ngx";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"],
 })
 export class AppComponent {
   constructor(
+    private appRate: AppRate,
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
@@ -22,6 +24,34 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      this.appRate.preferences = {
+        displayAppName: "Christian Matching",
+        usesUntilPrompt: 4,
+        promptAgainForEachNewVersion: true,
+        storeAppURL: {
+          ios: "",
+          android: "",
+        },
+        customLocale: {
+          title: "Do you enjoy %@?",
+          message:
+            "If you enjoy %@, would you mind taking a moment to rate it?",
+          cancelButtonLabel: "No thanks",
+          laterButtonLabel: "Remind me later!",
+          rateButtonLabel: "Rate It Now",
+        },
+        callbacks: {
+          onRateDialogShow: function (callback) {
+            console.log("rate shown");
+          },
+          onButtonClicked: function (buttonIndex) {
+            console.log("selected index: ", buttonIndex);
+          },
+        },
+      };
+
+      this.appRate.promptForRating(true);
     });
   }
 }
